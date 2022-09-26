4e8fccd29ce934779e427cf9f66845cca64b58c1
Wellington Correia Borsato Jr <wellington.borsato@yahoo.com.br>
2022-09-26 09:55:43 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Ghost)
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+})
+basic.showString("well")
+basic.forever(function () {
+	
+})
